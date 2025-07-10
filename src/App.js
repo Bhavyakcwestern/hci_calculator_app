19a19cf9d427d6f6427e1930b9161aa592f8cbdf
Bhavya Champaneri <bchampan@uwo.ca>
2025-07-10 01:59:54 -0400

    Bracket layout added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import { useReducer } from "react"
 import DigitButton from "./DigitButton"
 import OperationButton from "./OperationButton"
@@ -158,42 +159,59 @@ function App() {
 
   return (
     <div className="calculator-grid">
+      {/* display */}
       <div className="output">
         <div className="previous-operand">
           {formatOperand(previousOperand)} {operation}
         </div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
+
+      {/* row 1 */}
+      <button onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
         AC
       </button>
       <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
         DEL
       </button>
-      <OperationButton operation="÷" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="*" dispatch={dispatch} />
-      <DigitButton digit="4" dispatch={dispatch} />
-      <DigitButton digit="5" dispatch={dispatch} />
-      <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} />
+      <button
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: "(" } })
+        }
+      >
+        (
+      </button>
+      <button
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: ")" } })
+        }
+      >
+        )
+      </button>
+
+      {/* row 2 */}
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
+
+      {/* row 3 */}
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+
+      {/* row 4 */}
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
       <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
+
+      {/* row 5 */}
       <DigitButton digit="0" dispatch={dispatch} />
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-      >
-        =
-      </button>
+      <DigitButton digit="." dispatch={dispatch} />
+      <button onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
+      <OperationButton operation="+" dispatch={dispatch} />
     </div>
   )
 }
